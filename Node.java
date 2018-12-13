d3d833945551635eeabfc536df1696cb246af00b
Jason Zheng <zzheng3@stuy.edu>
2018-12-12 19:23:45 -0500

    added constructor

--- Node.java
+++ Node.java
@@ -1,4 +1,9 @@
 class Node{
  private int data;
  private Node next,prev;
+ public Node (int newData, Node newNext, Node newPrev) {
+   data = newData;
+   next = newNext;
+   prev = newPrev;
+ }
 }
