3224073cae6bda6e0eb757804764a4578a58c246
Jason Zheng <zzheng3@stuy.edu>
2018-12-12 19:26:23 -0500

    added get methods

--- Node.java
+++ Node.java
@@ -6,4 +6,13 @@ class Node{
    next = newNext;
    prev = newPrev;
  }
+ public int getData() {
+   return data;
+ }
+ public Node getNext() {
+   return next;
+ }
+ public Node getPrev() {
+   return prev;
+ }
 }
