404866870478c4f0921503d0d6e0892e203ac8e1
Jason Zheng <zzheng3@stuy.edu>
2018-12-16 22:50:06 -0500

    added comments and fixed boolean add

--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,18 +1,25 @@
 class MyLinkedList{
   private int size;
   private Node start,end;
-  public MyLinkedList(int newSize, Node newStart, Node newEnd) {
-    size = newSize;
-    start = newStart;
-    end = newEnd;
+  public MyLinkedList() {
+    size = 0; //Creates an empty list with length of zero
   }
   public int size() {
-    return size;
+    return size; //Returns the length of the list
   }
   public boolean add(int value) {
-    Node what = new Node();
-    size = size + 1;
-    end.setNext(newNext);
+    if (size == 0) { //If the list is only supposed to have one element
+      Node element = new Node(value, null, null); //There can be no previous or next elements
+      size = size + 1; //The size goes from zero to one
+      start = element; //The first node would be the element
+      end = element; //The last node would also be the element
+    } else { //What if the list were to have more than one element
+      Node element = new Node(value, null, end); //The is no next element but the previous element would be the old last element
+      size = size + 1; //Increase the size
+      end.setNext(element); //Change the constructor of the old last element
+      end = element; //New last element
+      return true; //This is a boolean function
+    }
   }
   public String toString() {
     String sad = "[";
